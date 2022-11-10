333a974376ca6409cfdee1cb8fced069cd4fcf7c
MidoriYakushiji <m.yakushiji1008@gmail.com>
2022-11-10 16:22:42 +0900

    本多さん分反映

--- resources/views/event/eventichirankojin.blade.php
+++ resources/views/event/eventichirankojin.blade.php
@@ -13,26 +13,26 @@
       @endcomponent
 
       {{-- 右全体(10) --}}
-      <div class="col-10">
+      <div class="col-10 p-event-right-content">
 
         {{-- 右のコンテンツrow(12) --}}
         <div class="row align-items-center">
           {{--  ページタイトル(8)  --}}
           <div class="col-8">
-              <div class="alert alert-warning" role="alert">
+              <div class="alert alert-warning event-text-lg" role="alert">
                 新着順 イベント募集投稿</div>
           </div>
           {{--  新規募集投稿ボタン(4)  --}}
           <div class="col-4">
-            <button type="button" class="btn btn-outline-warning">イベントの募集投稿をする</button>
+            <button type="button" class="btn btn-outline-warning event-text-sm">イベントの募集投稿をする</button>
           </div>
         </div>
 
         {{--  カードを表示row  --}}
-        <div class="row">
+        <div class="row p-eventcards">
 
           @foreach ($items as $item)
-            <div class="card" style="width: 13rem;">
+            <div class="card p-eventcards__card" style="width: 13rem;">
               <a href="#">
                 <img src="img/noimage.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -47,40 +47,47 @@
 
         {{-- 右下のコンテンツrow(12) --}}
 
-        <div class="row">
+        <div class="row p-event-right-content">
           <div class="col-8"></div>
           <div class="col-4">
          {{ $items->links() }}
           </div>
         </div>
 
-        {{--  テキストエリア例  --}}
-        {{--  <div class="mb-3"> 
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label> 
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>       
-        </div>   --}}
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Launch demo modal
+        </button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
   </div>
 @endsection
 
+<script>
+  var myModal = document.getElementById('myModal')
+  var myInput = document.getElementById('myInput')
 
-
-<style>
-  .row {
-    background-color: #87785A;
-    display: flex;
-    justify-content: center;
-  }
-  .col-10 {
-    padding: 20px 20px;
-  }
-  .card {
-    margin: 10px 10px;
-  }
-
-  a {
-    color: #04047B;
-  }
-</style>
+  myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+  })
+</script>
