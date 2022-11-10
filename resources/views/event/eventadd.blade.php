f76c54b50f817a9337d275762b1e8a5131e50cae
idumi777 <idumi0928@gmail.com>
2022-11-10 16:24:27 +0900

    猪俣さん分反映

--- resources/views/event/eventadd.blade.php
+++ resources/views/event/eventadd.blade.php
@@ -12,8 +12,31 @@
            <section>
             <h2>新着順　イベント募集投稿</h2>
             <div>
+                <table border="1">
+                    <tbody>
+                        <tr>
+                            <td colspan="3">（必須）イベントタイトル</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">場所</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">（必須）開始　2022/02/22</td>
+                            <td>開始時間</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">（必須）終了　2022/02/22</td>
+                            <td>終了時間</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">関連URL お申し込みはこちら</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">イベント内容記述</td>
+                        </tr>
+                    </tbody>
+                </table>
                 
-                  <p>これは例文です</p>
             <div>
                 <div>
                     
