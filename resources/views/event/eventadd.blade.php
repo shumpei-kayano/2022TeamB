132e3f6e2d3624065943bb3d9fc34727f963a531
idumi777 <idumi0928@gmail.com>
2022-11-14 15:06:15 +0900

    猪俣分反映

--- resources/views/event/eventadd.blade.php
+++ resources/views/event/eventadd.blade.php
@@ -9,27 +9,22 @@
         {{-- 左側のメニューバー --}}
         @component('components.left')@endcomponent
         {{-- 右側のイベント欄 --}}
-        <section class="eventadd">
+        <section class="eventadd p-event-right-content">
             <h2 class="eventadd__title">新着順　イベント募集投稿</h2>
             <div class="eventadd__content">
-                <div class="eventadd__left">
-                    <img src="img/noimage.jpg" class="card-img-top" alt="...">
-                    <input type="submit" value="登録" class='button'>
-                    <input type="submit" value="削除" class='button'><br>
-                    <input type="submit" value="編集投稿者に表示" class='button'><br>
-                    <input type="submit" value="削除投稿者に表示" class='button'><br>
-
-                
-                <button class="vertical">とても長い文字をボタンに入れてしまった場合</button>
-
-
-
-
-
-
+                <div class="eventadd__left ">
+                    <div class="eventadd__img-container">
+                        <img src="img/noimage.jpg" class="card-img-top" alt="...">
+                        <input type="submit" value="登録" class='button'>
+                        <input type="submit" value="削除" class='button'>
+                    </div>
+                    <div class="eventadd__btn-container">
+                        <input type="submit" value="編集" class='button'>
+                        <input type="submit" value="削除" class='button'>
+                    </div>
                 </div>
                 <div class="eventadd__right">
-                    <table border="1">
+                    <table class="eventadd__table">
                         <tbody>
                             <tr>
                                 <td colspan="3"><label for="exampleInputEmail1" class="form-label"></label>
@@ -43,18 +38,18 @@
                             </tr>
                             <tr>
                                 <td colspan="2"><label for="exampleInputEmail1" class="form-label"></label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="（必須）開始　2022/02/22">
+                                    <input type="date" class="form-control" id="exampleInputEmail1" placeholder="（必須）開始　2022/02/22">
                                     <div id="emailHelp" class="form-text" ></div></td>
                                 <td><label for="exampleInputEmail1" class="form-label"></label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="開始時間">
+                                    <input type="time" class="form-control" id="exampleInputEmail1" placeholder="開始時間">
                                     <div id="emailHelp" class="form-text" ></td>
                             </tr>
                             <tr>
                                 <td colspan="2"><label for="exampleInputEmail1" class="form-label"></label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="（必須）開始　2022/02/22">
+                                    <input type="date" class="form-control" id="exampleInputEmail1" placeholder="（必須）終了　2022/02/22">
                                     <div id="emailHelp" class="form-text" ></td>
                                 <td><label for="exampleInputEmail1" class="form-label"></label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="終了時間">
+                                    <input type="time" class="form-control" id="exampleInputEmail1" placeholder="終了時間">
                                     <div id="emailHelp" class="form-text" ></td>
                             </tr>
                             <tr>
@@ -71,9 +66,6 @@
                     </table>
                 </div>
             <div>
-                <div>
-                    
-            </div>
         </section>
     </div>
     </div>
