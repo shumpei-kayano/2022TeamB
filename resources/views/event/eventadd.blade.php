2fae40f5b535ec8f883fbf415224c9bebb75dd5c
idumi777 <idumi0928@gmail.com>
2022-11-14 09:58:59 +0900

    猪俣：11/11分反映

--- resources/views/event/eventadd.blade.php
+++ resources/views/event/eventadd.blade.php
@@ -5,44 +5,76 @@
 @endsection
 
 @section('main')
-    <div class="row">
-        @component('components.left')
-        @endcomponent
-        <div class="eventadd_right">
-           <section>
-            <h2>新着順　イベント募集投稿</h2>
-            <div>
-                <table border="1">
-                    <tbody>
-                        <tr>
-                            <td colspan="3">（必須）イベントタイトル</td>
-                        </tr>
-                        <tr>
-                            <td colspan="3">場所</td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">（必須）開始　2022/02/22</td>
-                            <td>開始時間</td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">（必須）終了　2022/02/22</td>
-                            <td>終了時間</td>
-                        </tr>
-                        <tr>
-                            <td colspan="3">関連URL お申し込みはこちら</td>
-                        </tr>
-                        <tr>
-                            <td colspan="3">イベント内容記述</td>
-                        </tr>
-                    </tbody>
-                </table>
+    <div class="eventadd-container">
+        {{-- 左側のメニューバー --}}
+        @component('components.left')@endcomponent
+        {{-- 右側のイベント欄 --}}
+        <section class="eventadd">
+            <h2 class="eventadd__title">新着順　イベント募集投稿</h2>
+            <div class="eventadd__content">
+                <div class="eventadd__left">
+                    <img src="img/noimage.jpg" class="card-img-top" alt="...">
+                    <input type="submit" value="登録" class='button'>
+                    <input type="submit" value="削除" class='button'><br>
+                    <input type="submit" value="編集投稿者に表示" class='button'><br>
+                    <input type="submit" value="削除投稿者に表示" class='button'><br>
+
                 
+                <button class="vertical">とても長い文字をボタンに入れてしまった場合</button>
+
+
+
+
+
+
+                </div>
+                <div class="eventadd__right">
+                    <table border="1">
+                        <tbody>
+                            <tr>
+                                <td colspan="3"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="(必須)イベントタイトル">
+                                    <div id="emailHelp" class="form-text" ></div></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="場所">
+                                    <div id="emailHelp" class="form-text" ></div></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="（必須）開始　2022/02/22">
+                                    <div id="emailHelp" class="form-text" ></div></td>
+                                <td><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="開始時間">
+                                    <div id="emailHelp" class="form-text" ></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="（必須）開始　2022/02/22">
+                                    <div id="emailHelp" class="form-text" ></td>
+                                <td><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="終了時間">
+                                    <div id="emailHelp" class="form-text" ></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="関連URL お申し込みはこちら">
+                                    <div id="emailHelp" class="form-text" ></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><label for="exampleInputEmail1" class="form-label"></label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="イベント内容記述">
+                                    <div id="emailHelp" class="form-text" ></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             <div>
                 <div>
                     
             </div>
-           </section>
-        </div>
+        </section>
     </div>
     </div>
 @endsection
