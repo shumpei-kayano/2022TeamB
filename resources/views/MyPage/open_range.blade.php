579ea18353a93ec8e6ff27e031be418e38a90e6d
yuho0104 <tounaiyuho0104@icloud.com>
2022-12-02 16:29:02 +0900

    close1202

--- resources/views/MyPage/open_range.blade.php
+++ resources/views/MyPage/open_range.blade.php
@@ -12,8 +12,9 @@
 <div class="p-login_bg">
     <div class="p-login_bg__card_bg">   {{-- ログイン画面などの背景色 --}}
         <div class="p-open_range">
-            <br><h3>公開範囲の設定<img src="./img/close.png" width="80" height="60" border="0" style="opacity: 0.5"></h3>
-            <br><a href="#"><button class="p-open_range__b"><img src="./img/ダウンロード.png" width="80" height="60" border="0" style="opacity: 0.5"> 全体に公開</button></a>
+            <a href="{{URL::to('/')}}"><img src={{asset('/img/close.png')}} alt="" height="50px" width="40px" class="p-small_bg__logo_img"></a>
+            <br><h3>公開範囲の設定</h3>
+            <br><a href="#"><button class="p-open_range__b"><img src="./img/ダウンロード.png" width="65" height="50" border="0" style="opacity: 0.5"> 全体に公開</button></a>
             <br><a href="#"><button class="p-open_range__t"><img src="./img/double human.svg" width="50" height="30" border="0"> フォロワーだけに公開</button></a>
             <br><a href="#"><button class="p-open_range__n"><img src="./img/onehuman.png" width="80" height="60"> 自分だけに公開</button></a>
             <br><a href="#"><button class="p-open_range__navybtn">保存する</button></a>
