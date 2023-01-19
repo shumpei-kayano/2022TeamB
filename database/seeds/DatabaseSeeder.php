4e4684bb31dd67e63583e51fe0f5cfcc8e8006ce
idumi777 <idumi0928@gmail.com>
2023-01-19 12:07:48 +0900

    20230119inomata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(EventsTableSeeder::class);
-        // $this->call(PublicUsersTableSeeder::class);
+        //$this->call(EventsTableSeeder::class);
+        $this->call(PublicUsersTableSeeder::class);
         // $this->call(AreasTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
