262e820340fce4ee0884911be67df06e4145823d
hayateeeeee <ooi2172026@stu.oa-hara.ac.jp>
2023-02-09 14:56:25 +0900

    0209okano

--- resources/views/open_chat/join_open_chat.blade.php
+++ resources/views/open_chat/join_open_chat.blade.php
@@ -18,10 +18,9 @@
         
      
     <div class="p-new_create_block">
-       <div class="p-new_create_block__space"></div>
         <div class="p-new_create_block__display">
         <h1>{{ $data->title }}</h1><br>
-        <div class="chat_preview_detail"><br><h5>{{ $data->room_detail }}</h5><br></div><br>
+        <div class="chat_preview_detail"><br><p>{{ $data->room_detail }}</p><br></div><br>
         <form action="/open_chat_room/{{ $data->id }}">
             <input class="p-large_bg__check_btn" type="submit" value="参加する">
         </form>
